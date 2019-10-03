0d41337e62751c4b0c0ede25b3a4af65c6fea73b
Jordan Earls <earlz@earlz.net>
2019-10-02 19:17:04 -0600

    Fix bug in macro, rename to __init_neutron and add panic handler here

--- src/lib.rs
+++ src/lib.rs
@@ -4,4 +4,16 @@ pub mod testing;
 
 //nothing needed in this yet
 #[no_mangle]
-pub extern "C" fn __init_qtum() {}
+pub extern "C" fn __init_neutron() {}
+
+
+extern crate neutron_star_rt;
+
+#[panic_handler]
+pub fn _neutron_panic_handler(_info: &core::panic::PanicInfo) -> ! {
+    use neutron_star_rt::__exit;
+    unsafe{
+        //return fault + error + revert
+        __exit(8 + 1 + 2)
+    }
+}
