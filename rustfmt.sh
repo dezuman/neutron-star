040f05dfd94aaeb3319fd155d32c1ed091f1cba8
dezuman <johannes.borg@hotmail.com>
2021-04-27 17:35:20 +0200

    Add rustfmt script and run it

---
+++ rustfmt.sh
@@ -0,0 +1,3 @@
+# This script runs rustfmt for the parts of the repo that uses it
+
+rustfmt ./src/syscalls.rs
--- src/syscalls.rs
+++ src/syscalls.rs
@@ -4,42 +4,41 @@
 //! These functions only do the basic methods of abstraction, such as returning results and pairs
 //! instead of taking mutable pointers for integers
 
-extern crate neutron_star_rt;
 extern crate neutron_common;
+extern crate neutron_star_rt;
 
-use neutron_star_rt::*;
-use neutron_common::*;
 use core::mem::transmute;
-
+use neutron_common::*;
+use neutron_star_rt::*;
 
 #[derive(core::fmt::Debug)]
-pub enum SystemError{
+pub enum SystemError {
     Generic(u32),
     UnexpectedSize,
 }
 
-pub fn push_costack(data: &[u8]){
-    unsafe{
+pub fn push_costack(data: &[u8]) {
+    unsafe {
         let size = data.len();
         let ptr = data.as_ptr();
         __push_costack(ptr, size);
     }
 }
 
-pub fn pop_costack_fixed(buffer: &mut [u8]) -> Result<u32, SystemError>{
-    unsafe{
+pub fn pop_costack_fixed(buffer: &mut [u8]) -> Result<u32, SystemError> {
+    unsafe {
         let size = buffer.len();
         let ptr = buffer.as_mut_ptr();
         let actual_size = __pop_costack(ptr, size) as u32;
-        if actual_size > 0x8000_0000{
+        if actual_size > 0x8000_0000 {
             Err(SystemError::Generic(actual_size))
-        }else{
+        } else {
             Ok(actual_size)
         }
     }
 }
-pub fn discard_costack(){
-    unsafe{
+pub fn discard_costack() {
+    unsafe {
         __pop_costack(core::ptr::null_mut(), 0);
     }
 }
@@ -62,117 +61,106 @@ pub fn pop_sccs() -> Result<Vec<u8>, SystemError>{
 }
 */
 
-/// Pops an exactly u64 value from the stack. 
-pub fn pop_costack_u64() -> Result<u64, SystemError>{
-    const SIZE:usize = 8;
+/// Pops an exactly u64 value from the stack.
+pub fn pop_costack_u64() -> Result<u64, SystemError> {
+    const SIZE: usize = 8;
     let mut buffer = [0; SIZE];
     let result = pop_costack_fixed(&mut buffer)?;
-    if result != SIZE as u32{
+    if result != SIZE as u32 {
         return Err(SystemError::UnexpectedSize);
     }
     return Ok(unsafe { transmute::<[u8; SIZE], u64>(buffer) }.to_le());
 }
 
-/// Pops an exactly u32 value from the stack. 
-pub fn pop_costack_u32() -> Result<u32, SystemError>{
-    const SIZE:usize = 4;
+/// Pops an exactly u32 value from the stack.
+pub fn pop_costack_u32() -> Result<u32, SystemError> {
+    const SIZE: usize = 4;
     let mut buffer = [0; SIZE];
     let result = pop_costack_fixed(&mut buffer)?;
-    if result != SIZE as u32{
+    if result != SIZE as u32 {
         return Err(SystemError::UnexpectedSize);
     }
     return Ok(unsafe { transmute::<[u8; SIZE], u32>(buffer) }.to_le());
 }
 
-/// Pops an exactly u16 value from the stack. 
-pub fn pop_costack_u16() -> Result<u16, SystemError>{
-    const SIZE:usize = 2;
+/// Pops an exactly u16 value from the stack.
+pub fn pop_costack_u16() -> Result<u16, SystemError> {
+    const SIZE: usize = 2;
     let mut buffer = [0; SIZE];
     let result = pop_costack_fixed(&mut buffer)?;
-    if result != SIZE as u32{
+    if result != SIZE as u32 {
         return Err(SystemError::UnexpectedSize);
     }
     return Ok(unsafe { transmute::<[u8; SIZE], u16>(buffer) }.to_le());
 }
 
-/// Pops an exactly u8 value from the stack. 
-pub fn pop_costack_u8() -> Result<u8, SystemError>{
-    const SIZE:usize = 1;
+/// Pops an exactly u8 value from the stack.
+pub fn pop_costack_u8() -> Result<u8, SystemError> {
+    const SIZE: usize = 1;
     let mut buffer = [0];
     let result = pop_costack_fixed(&mut buffer)?;
-    if result != SIZE as u32{
+    if result != SIZE as u32 {
         return Err(SystemError::UnexpectedSize);
     }
     return Ok(buffer[0]);
 }
-/// Pops a NeutronShortAddress value from the stack. 
-pub fn pop_costack_address() -> Result<NeutronAddress, SystemError>{
-    const SIZE:usize = core::mem::size_of::<NeutronAddress>();
+/// Pops a NeutronShortAddress value from the stack.
+pub fn pop_costack_address() -> Result<NeutronAddress, SystemError> {
+    const SIZE: usize = core::mem::size_of::<NeutronAddress>();
     let mut buffer = [0; SIZE];
     let result = pop_costack_fixed(&mut buffer)?;
-    if result != SIZE as u32{
+    if result != SIZE as u32 {
         return Err(SystemError::UnexpectedSize);
     }
-    return Ok(unsafe { transmute::<[u8; SIZE], NeutronAddress>(buffer)});
+    return Ok(unsafe { transmute::<[u8; SIZE], NeutronAddress>(buffer) });
 }
 
-/// Pushes a NeutronShortAddress to the stack. 
-pub fn push_costack_address(value: &NeutronAddress){
-    const SIZE:usize = core::mem::size_of::<NeutronAddress>();
-    let t = unsafe{
-        transmute::<NeutronAddress, [u8; SIZE]>(*value)
-    };
+/// Pushes a NeutronShortAddress to the stack.
+pub fn push_costack_address(value: &NeutronAddress) {
+    const SIZE: usize = core::mem::size_of::<NeutronAddress>();
+    let t = unsafe { transmute::<NeutronAddress, [u8; SIZE]>(*value) };
     push_costack(&t);
 }
 
-/// Pushes a u64 value to the stack. 
-pub fn push_costack_u64(value: u64){
-    const SIZE:usize = 8;
-    let t = unsafe{
-        transmute::<u64, [u8; SIZE]>(value)
-    };
+/// Pushes a u64 value to the stack.
+pub fn push_costack_u64(value: u64) {
+    const SIZE: usize = 8;
+    let t = unsafe { transmute::<u64, [u8; SIZE]>(value) };
     push_costack(&t);
 }
-/// Pushes a u32 value to the stack. 
-pub fn push_costack_u32(value: u32){
-    const SIZE:usize = 4;
-    let t = unsafe{
-        transmute::<u32, [u8; SIZE]>(value)
-    };
+/// Pushes a u32 value to the stack.
+pub fn push_costack_u32(value: u32) {
+    const SIZE: usize = 4;
+    let t = unsafe { transmute::<u32, [u8; SIZE]>(value) };
     push_costack(&t);
 }
 
-/// Pushes a u64 value to the stack. 
-pub fn push_costack_u16(value: u16){
-    const SIZE:usize = 2;
-    let t = unsafe{
-        transmute::<u16, [u8; SIZE]>(value)
-    };
+/// Pushes a u64 value to the stack.
+pub fn push_costack_u16(value: u16) {
+    const SIZE: usize = 2;
+    let t = unsafe { transmute::<u16, [u8; SIZE]>(value) };
     push_costack(&t);
 }
 
-/// Pushes a u64 value to the stack. 
-pub fn push_costack_u8(value: u8){
-    const SIZE:usize = 1;
-    let t = unsafe{
-        transmute::<u8, [u8; SIZE]>(value)
-    };
+/// Pushes a u64 value to the stack.
+pub fn push_costack_u8(value: u8) {
+    const SIZE: usize = 1;
+    let t = unsafe { transmute::<u8, [u8; SIZE]>(value) };
     push_costack(&t);
 }
 
-pub fn get_self_address() -> NeutronAddress{
+pub fn get_self_address() -> NeutronAddress {
     //TODO
     return NeutronAddress::default();
 }
 
-pub fn _system_call(element: u32, function: u32) -> Result<u32, SystemError>{
-    unsafe{
+pub fn _system_call(element: u32, function: u32) -> Result<u32, SystemError> {
+    unsafe {
         let result = __system_call(element, function);
-        if result >= 0x8000_0000{
+        if result >= 0x8000_0000 {
             Err(SystemError::Generic(result))
-        }else{
+        } else {
             Ok(result)
         }
     }
 }
-
